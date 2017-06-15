5f474b1c26c0ea91b12f3e01b22fc2f4b818652f
rishabh-malik <rishabhmalik249@gmail.com>
2017-06-15 09:58:28 -0400

    lemmatizing in nlp

---
+++ lemmatizing.py
@@ -0,0 +1,17 @@
+#Simimlar to stemming .Major difference is stemming can often create non-existent words, whereas lemmas are actual words.
+
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+
+print(lemmatizer.lemmatize("cats"))
+print(lemmatizer.lemmatize("cacti"))
+print(lemmatizer.lemmatize("geese"))
+print(lemmatizer.lemmatize("rocks"))
+print(lemmatizer.lemmatize("python"))
+print(lemmatizer.lemmatize("better", pos="a"))
+print(lemmatizer.lemmatize("best", pos="a"))
+print(lemmatizer.lemmatize("run"))
+print(lemmatizer.lemmatize("run",'v'))
+
+
